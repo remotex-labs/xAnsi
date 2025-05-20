8e2647e4bc90672d5f2ded2f4cb869b4b5574f06
Garefild <Garefild2@gmail.com>
2025-05-20 08:16:03 +0300

    Initialize TypeScript and custom build system

---
+++ xbuild.config.ts
@@ -0,0 +1,50 @@
+/**
+ * Import will remove at compile time
+ */
+
+import type { xBuildConfig } from '@remotex-labs/xbuild';
+
+/**
+ * Imports
+ */
+
+import { version } from 'process';
+import pkg from './package.json' with { type: 'json' };
+
+/**
+ * Config build
+ */
+
+const config: Array<xBuildConfig> = [
+    {
+        bundleDeclaration: true,
+        esbuild: {
+            bundle: true,
+            minify: true,
+            format: 'esm',
+            outdir: 'dist/esm',
+            target: [ `node${ version.slice(1) }` ],
+            platform: 'node',
+            packages: 'external',
+            sourcemap: true,
+            sourceRoot: `https://github.com/remotex-labs/xAnsi/tree/v${ pkg.version }/`,
+            entryPoints: {
+                'index': 'src/index.ts',
+                'shadow.service': 'src/services/shadow.service.ts',
+                'ansi.component': 'src/components/ansi.component.ts',
+                'xterm.component': 'src/components/xterm.component.ts'
+            }
+        }
+    },
+    {
+        noTypeChecker: true,
+        bundleDeclaration: false,
+        esbuild: {
+            bundle: true,
+            format: 'cjs',
+            outdir: 'dist/cjs'
+        }
+    }
+];
+
+export default config;
